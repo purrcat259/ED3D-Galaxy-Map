d977c6d3858eb444139b790b9f3f25dbb814acb0
Simon AM <simon@agius-muscat.net>
2017-10-19 21:19:13 +0200

    Build server along with demo

--- gulp/scripts.js
+++ gulp/scripts.js
@@ -42,7 +42,7 @@ gulp.task('build', () => {
 
 let demoFile = './js/demo.js';
 
-gulp.task('build_demo', ['build'], () => {
+gulp.task('build_demo', ['build', 'build_server'], () => {
     let b = browserify(demoFile);
     return b.transform(babelify)
         .bundle()
